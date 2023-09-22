24b404aed14644b25a8fab37dc4eab7d8c1c50b6
Leandro Sousa Costa <118322766+LeannonS@users.noreply.github.com>
2023-09-21 22:13:46 -0300

    Update Utils.hpp

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -15,7 +15,7 @@ class Utils
     unordered_set<wstring> getInput(const locale& loc);
     void swapMinHeap(vector<pair<wstring, int>>& minHeap, const int& indexToRemove, const vector<pair<wstring, int>>& proxTopK, const int& indexMinHeap);
     void makeFrequenciesMaps(const locale& loc, const int& numFiles, vector<unordered_map<wstring, int>>& frequenceMap);
-    void printHeap(vector<pair<wstring, int>> heap);
+    // void printHeap(vector<pair<wstring, int>> heap);
     void makeHeaps(const int& k, const int& numFiles, const vector<unordered_map <wstring, int>>& frequenceMap, vector<vector<pair<wstring, int>>>& minHeap, vector<pair<wstring, int>>& proxTopK);
 };
 
